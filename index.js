3c4fd4fc978a4c48e0212776564e575cb254b105
pierre.juif.proxistore <pierre.juif@proxistore.com>
2023-11-24 13:16:33 +0100

    finished

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
+// nav change on scroll
+
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(window).scrollTop() >= 100) {
-            $('.navbar').removeClass('bg-transparent');
-            $('.navbar').addClass('bg-white');
+        var scrollPosition = $(window).scrollTop();
+        var triggerHeight = $(window).height() * 0.8;
+
+        if (scrollPosition >= triggerHeight) {
+            $('.nav-container').removeClass('bg-primary');
+            $('.nav-container').addClass('bg-white');
+            $('.navbar .nav-link').each(function () {
+                $(this).removeClass('text-white');
+            });
+            $('.navbar .btn.btn-light:not(.bg-transparent)').removeClass('btn-light').addClass('btn-primary');
+            $('.navbar .btn.btn-secondary.bg-transparent').removeClass('btn-secondary').addClass('btn-light');
+            $('.navbar-brand .logo-white').removeClass('d-inline-block').addClass('d-none');
+            $('.navbar-brand .logo-color').removeClass('d-none').addClass('d-inline-block');
         } else {
-            $('.navbar').removeClass('bg-white');
-            $('.navbar').addClass('bg-transparent');
+            $('.nav-container').removeClass('bg-white');
+            $('.nav-container').addClass('bg-primary');
+            $('.navbar .nav-link').each(function () {
+                $(this).addClass('text-white');
+            });
+            $('.navbar .btn.btn-primary').removeClass('btn-primary').addClass('btn-light');
+            $('.navbar .btn.btn-light.bg-transparent').removeClass('btn-light').addClass('btn-secondary');
+            $('.navbar-brand .logo-color').removeClass('d-inline-block').addClass('d-none');
+            $('.navbar-brand .logo-white').removeClass('d-none').addClass('d-inline-block');
         }
     });
 });
+
+//scroll revelator
+$(document).ready(function() {
+    // Fonction pour détecter si un élément avec la classe fadeInLeftDiv est partiellement visible à l'écran
+    function isPartiallyInView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+        var threshold = 200; // Ajustez cette valeur pour déclencher l'animation plus tôt
+
+        return ((elemBottom <= docViewBottom + threshold) && (elemTop >= docViewTop - threshold));
+    }
+
+    // Fonction pour ajouter la classe d'animation lorsque l'élément avec la classe fadeInLeftDiv est partiellement visible
+    function addAnimationClass() {
+        $(".fadeInLeftRevelator, .fadeInRightRevelator, .fadeInBottomRevelator").each(function() {
+            if (isPartiallyInView(this)) {
+                $(this).removeClass("hidden").addClass("animate__animated");
+                if ($(this).hasClass("fadeInLeftRevelator")) {
+                    $(this).addClass("animate__fadeInLeft");
+                } else if ($(this).hasClass("fadeInRightRevelator")) {
+                    $(this).addClass("animate__fadeInRight");
+                } else if ($(this).hasClass("fadeInBottomRevelator")) {
+                    $(this).addClass("animate__fadeInUp");
+                }
+            }
+        });
+    }
+    $(window).on('scroll', addAnimationClass);
+    addAnimationClass();
+});
+
+//fill heart
+function heartFill() {
+    //changement de class bi-heart-fill
+}
